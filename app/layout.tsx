54960ca5cd83c9b215f16d4c17674ffb280fd425
aaaravv <aaravmishra619@gmail.com>
2023-07-23 22:51:28 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Prometheus AI",
+  description: "An AI platform.",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
